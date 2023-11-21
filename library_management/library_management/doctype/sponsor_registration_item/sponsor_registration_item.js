d647b3014265a1bd8f521f2440394a5f2162f08b
rampriya <rampriyaj@thirvusoft.in>
2023-11-21 12:47:07 +0530

    All docTypes added

---
+++ library_management/library_management/doctype/sponsor_registration_item/sponsor_registration_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Faris Ansari and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sponsor Registration Item', {
+	// refresh: function(frm) {
+
+	// }
+});
